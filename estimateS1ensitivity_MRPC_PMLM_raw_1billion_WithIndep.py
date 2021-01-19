a8ae90c896177d693c6a4031004737b1956946fa
Michael Hahn <mhahn29@gmail.com>
2021-01-18 19:22:46 -0800

    code

---
+++ estimateS1ensitivity_MRPC_PMLM_raw_1billion_WithIndep.py
@@ -0,0 +1,199 @@
+import math
+import sys
+import torch
+task = sys.argv[1]
+
+assert task == "MRPC"
+
+def mean(values):
+   return sum(values)/len(values)
+
+sensitivityHistogram = [0 for _ in range(40)]
+
+
+from scipy.optimize import linprog
+
+
+def getMaxOverPartitions(A, b, x_bounds, perSubsetSensitivities):
+   #print(perSubsetSensitivities)
+   c = [-x for x in perSubsetSensitivities]
+   res = linprog(c, A_ub=A, b_ub=b, bounds=x_bounds)
+   # find the highly sensitive partition
+   return -res.fun
+from random import shuffle
+
+alternatives_predictions_binary = {}
+alternatives_predictions_float = {}
+predictions_all = []
+
+
+for group in ["", "_Independent"]:
+ with open(f"/u/scr/mhahn/PRETRAINED/GLUE/glue_data/MRPC/dev_alternatives_predictions_PMLM_1billion_raw{group}.tsv", "r", encoding='utf-8') as inFile:
+  for line in inFile:
+     if len(line) < 5:
+       continue
+     line = line.strip().split("\t")
+     if len(line) == 2:
+       line.append("0.0")
+     sentence, cont, binary = line
+     cont = float(cont)
+     assert cont <= 0.0
+     alternatives_predictions_binary[sentence.strip()] = int(binary.strip())
+     alternatives_predictions_float[sentence.strip()] = cont
+     predictions_all.append(cont)
+  print(len(alternatives_predictions_binary))
+#quit()
+
+predictions_all = torch.FloatTensor(predictions_all)
+variance_predictions = predictions_all.pow(2).mean(dim=0) - predictions_all.mean(dim=0).pow(2)
+print(predictions_all)
+print(predictions_all.mean(dim=0))
+print(variance_predictions)
+
+alternatives = []
+#quit()
+
+for group in ["", "_OnlySubsetsNoAlternatives"]:
+ with open(f"/u/scr/mhahn/PRETRAINED/GLUE/glue_data/MRPC/dev_alternatives_c{group}.tsv", "r", encoding='utf-8') as inFile:
+  alternatives += inFile.read().strip().split("#####\n")
+  print(len(alternatives))
+
+
+
+from collections import defaultdict
+
+RoBERTa_alternatives = defaultdict(list)
+for group in ["", "_Independent"]: # , "_d", "_e"
+ with open(f"/u/scr/mhahn/PRETRAINED/GLUE/glue_data/MRPC/dev_alternatives_PMLM_1billion_raw{group}.tsv", "r") as inFile:
+  for line in inFile:
+     line = line.strip().split("\t")
+     if len(line) < 3:
+           print("ERROR", line)
+           continue
+     RoBERTa_alternatives[(line[0].strip(), line[1].strip())].append(line[2])
+
+
+
+sensitivities = []
+
+
+processed = set()
+
+with open(f"/u/scr/mhahn/sensitivity/sensitivities/s1ensitivities_{__file__}", "w") as outFile:
+ print("Original", "\t", "BinaryS1ensitivity", file=outFile)
+ for alternative in alternatives:
+   if len(alternative) < 5:
+      continue
+   variants_set = set()
+   variants_dict = {}
+   
+   alternative = alternative.split("\n")
+   original = alternative[0].strip()
+   print(original)
+   questionMarks = [int(x) for x in alternative[1].split(" ")]
+
+   tokenizedBare = alternative[2].strip()
+   tokenized = alternative[2].strip().split(" ")
+
+
+   tokenized1 = tokenized[:questionMarks[0]]
+   tokenized2 = tokenized[questionMarks[0]:]
+
+   tokenizeds = [tokenized1, tokenized2]
+   for i in range(2):
+       tokenizeds[i] = ("".join(tokenizeds[i])).replace("▁", " ").replace("</s>", "").strip()
+   tokenizedPairResult = tuple(tokenizeds) 
+   original = tokenizedPairResult[0] + "@ " + tokenizedPairResult[1]
+
+   print(original)
+#   assert original in itemsPredictions
+#   entry = itemsPredictions[original]
+#   predictionForOriginal = torch.FloatTensor([float(x) for x in entry[2].split(" ")]).exp()
+#   assert predictionForOriginal <= 1, entry
+   #print(predictionForOriginal)
+   #quit()
+
+   hasConsideredSubsets = set()
+
+   for variant in alternative[3:]:
+      #print(variant)
+      if len(variant) < 5:
+         continue
+      try:
+         subset, sentence= variant.strip().split("\t")
+      except ValueError:
+        continue
+      subset = subset.strip()
+      sentence = sentence.split()
+   #   print("SENTENCE AS FOUND", sentence)
+      assert (subset,tokenizedBare) in RoBERTa_alternatives, (subset,tokenizedBare)
+
+      if subset in hasConsideredSubsets:
+        continue
+      hasConsideredSubsets.add(subset)
+      for sentence in RoBERTa_alternatives[(subset,tokenizedBare)]:
+          sentence = sentence.strip()
+#          print(sentence)
+          variants_set.add(sentence)
+          assert sentence in alternatives_predictions_binary, sentence
+          if subset not in variants_dict:
+             variants_dict[subset] = []
+          variants_dict[subset].append(sentence)
+   print(len(variants_set), "variants")
+   valuesPerVariant = {}
+   for variant in variants_set:
+   #  print(variant)
+     try:
+       assert alternatives_predictions_binary[variant] in [0, 1], alternatives_predictions_binary[variant]
+       valuesPerVariant[variant] = alternatives_predictions_float[variant] 
+     except ValueError:
+        print("VALUE ERROR", variant)
+        valuesPerVariant[variant] = 0
+     except AttributeError:
+        print("VALUE ERROR", variant)
+        valuesPerVariant[variant] = 0
+
+   varianceBySubset = {}
+   for subset in variants_dict:
+       values = torch.FloatTensor([ valuesPerVariant[x] for x in variants_dict[subset]]).exp()
+       varianceBySubset[subset] = 4*float((values.mean(dim=0) - values).pow(2).mean(dim=0).max())
+       assert varianceBySubset[subset] <= 1
+
+
+   subsetsEnumeration = list(variants_dict)
+   if len(subsetsEnumeration) == 0:
+     continue 
+   N = len(subsetsEnumeration[0])
+   A = [[0 for subset in range(len(subsetsEnumeration))] for inp in range(N)]
+   for inp in range(N):
+       for subset, bitstr in enumerate(subsetsEnumeration):
+          assert len(bitstr) == N
+          if bitstr[inp] == "1":
+              A[inp][subset] = 1
+   
+   
+   b = [1 for _ in range(N)]
+   x_bounds = [(0,1) for _ in range(len(subsetsEnumeration))]
+   perSubsetSensitivities = [varianceBySubset[x] for x in subsetsEnumeration]
+
+   sensitivity = getMaxOverPartitions(A, b, x_bounds, perSubsetSensitivities)
+   print("OVERALL SENSITIVITY ON THIS DATAPOINT", sensitivity)
+   try:
+      sensitivityHistogram[int(2*sensitivity)] += 1
+   except IndexError:
+      print("Index Error")
+   sensitivities.append(sensitivity)
+   print("Average block sensitivity of the model", sum(sensitivities)/len(sensitivities))
+   print(original, "\t", sensitivity, file=outFile)
+
+print("Examples", len(sensitivities))
+print("Average block sensitivity of the model", sum(sensitivities)/len(sensitivities))
+print("Median block sensitivity of the model", sorted(sensitivities)[int(len(sensitivities)/2)])
+variance = sum([x**2 for x in sensitivities]) / len(sensitivities) - (sum(sensitivities)/len(sensitivities))**2
+import math
+print("Standard error", variance/math.sqrt(len(sensitivities)))
+
+sensitivityHistogram = torch.FloatTensor(sensitivityHistogram)
+print(sensitivityHistogram/sensitivityHistogram.sum())
+
+
---
+++ getPredictionsWSCAlternatives_PMLM_raw_1billion_Independent.py
@@ -0,0 +1,133 @@
+"""
+Evaluate average block sensitivity for WSC.
+"""
+import traceback
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+detokenizer = TreebankWordDetokenizer()
+
+from collections import defaultdict
+
+fromOriginal = {}
+with open("/u/scr/mhahn/PRETRAINED/WSC/val_alternatives_c_OnlySubsetsNoAlternatives.txt", "r") as inFile: # _SECOND_VERSION_BACKUP
+  alternatives = [x.split("\n") for x in inFile.read().strip().split("#####\n")]
+for alt in alternatives:
+  if len(alt) < 3:
+      continue
+  original = alt[0].strip()
+  boundaries = [int(x) for x in alt[1].strip().split(" ")]
+  tokenized = alt[2].strip()
+  identifier = tokenized+"@REFERENTS@"+"@".join([str(x) for x in sorted(boundaries)])
+  fromOriginal[identifier] = (original, boundaries)
+
+print(len(fromOriginal))
+print(len(alternatives))
+
+from fairseq.models.roberta import RobertaModel
+from examples.roberta.wsc import wsc_utils  # also loads WSC task and criterion
+from examples.roberta.wsc import wsc_task
+#roberta = RobertaModel.from_pretrained('checkpoints', 'checkpoint_best.pt', 'WSC/')
+nsamples, ncorrect = 0, 0
+
+def mean(values):
+   return sum(values)/len(values)
+
+sensitivityHistogram = [0 for _ in range(40)]
+
+
+def variance(values):
+   return mean([x**2 for x in values]) - mean(values)**2
+
+roberta = RobertaModel.from_pretrained('/u/scr/mhahn/PRETRAINED/roberta.large.wsc', "model.pt", "/u/scr/mhahn/PRETRAINED/WSC/")
+roberta.cuda()
+
+
+
+from random import shuffle
+
+evaluatedSoFar = set()
+
+hasEvaluated = set()
+
+with open(f"/u/scr/mhahn/PRETRAINED/WSC/val_alternatives_predictions_PMLM_1billion_raw_Independent.txt", "w") as outFile:
+ with open("/u/scr/mhahn/PRETRAINED/WSC/val_alternatives_PMLM_1billion_raw_Independent.tsv", "r") as inFile: # _SECOND_VERSION_BACKUP
+  while True:
+   try:
+      line = next(inFile).strip()
+   except StopIteration:
+      print("File end", 62)
+      break
+   try:
+      subset, original_tokenized, alternative = line.strip().split("\t")
+   except ValueError:
+      print("ValueError: ", line)
+      continue
+   if len(alternative) < 5:
+      continue
+
+
+   # this is for cases where stuff appears after [SEP]. Does not seem to happen in /u/scr/mhahn/PRETRAINED/WSC/val_alternatives_PMLM_1billion_raw.tsv
+   # The script was run without the next three lines, I inserted them afterwards without testing.
+   alternative = alternative.split("[SEP]")
+   assert len(alternative) >= 1, alternative
+   assert len(alternative[0]) > 5, alternative
+   alternative = alternative[0]
+
+
+   variants_set = set()
+   variants_dict = {}
+   original_tokenized_list=original_tokenized.strip().split(" ")
+   toNPs = {}
+   alternative = alternative.replace("<s>", "").replace("</s>", "").strip()
+   #print(original_tokenized)
+   assert (original_tokenized.strip() in fromOriginal), original_tokenized
+   _, boundaries = (fromOriginal[original_tokenized.strip()])
+   if alternative in hasEvaluated:
+      continue
+   hasEvaluated.add(alternative)
+#   print(alternative, fromOriginal[original_tokenized.strip()])
+   alternativeOriginal = alternative
+
+   alternative = alternative.replace("[CLS]", "").replace("[SEP]", "").strip().replace(" ' s ", " 's ").replace(" ' ll ", " 'll ").replace(" ' d ", " 'd ").replace("n ' t ", "n't ").replace(" ' ve ", " 've ").replace(" @ - @ ", "-").replace("( ", "(").replace(" ' re ", " 're ")
+
+   if len([_ for i in alternative if i == "_"]) != 2:
+      print("ADDITIONAL UNDERSCORE, SKIPPING", alternative)
+      continue
+   print("...")
+#   print(alternative)
+   alternative = alternative[:alternative.index("_")] + "FIRSTBRACKET" + alternative[alternative.index("_")+1:]
+   alternative = alternative[:alternative.index("_")] + "SECONDBRACKET" + alternative[alternative.index("_")+1:]
+
+   print(alternative)
+   alternative = detokenizer.detokenize(alternative.split(" "))
+   alternative = alternative.replace("FIRSTBRACKET ", " _").replace(" SECONDBRACKET", "_ ")
+   alternative = alternative.replace("FIRSTBRACKET", " _").replace("SECONDBRACKET", "_ ")
+   alternative = alternative.replace("  ", " ")
+   print(alternative)
+   variant = alternative.replace(" [ ", " [") #.replace(" _ ", " _")
+   if variant.startswith("_") and variant[1] == " ":
+     variant = "_"+variant[2:]
+   assert " _ " not in " "+variant+" ", variant
+     #print(variant)
+   if "] " not in variant: # Occurs occasionally: ]'re
+      variant = variant.replace("]", "] ")
+   try:
+     value = 1 if roberta.disambiguate_pronoun(variant) == True else -1
+     print(alternativeOriginal, "\t", value, file=outFile)
+     if len(hasEvaluated) % 100 == 0:
+       print(variant, len(hasEvaluated))
+   except ValueError:
+      print("VALUE ERROR", variant)
+#      valuesPerVariant[variant] = 0
+   except AttributeError:
+      print("ATTRIBUTE ERROR", variant)
+ #     valuesPerVariant[variant] = 0
+   except RuntimeError:
+      print("RUNTIME ERROR", variant)
+  #    valuesPerVariant[variant] = 0
+   except Exception as e:
+      print("EXCEPTION ", variant)
+      print(traceback.print_exc())
+      print(e)
+#      assert False
+   #     valuesPerVariant[variant] = 0
+
